fb06f6e32d33c174589dd4780b59a8bca9ace321
Lovejoy (mt) <lovejoy.chris@gmail.com>
2013-01-30 19:06:32 -0800

    updated style, improved structure of changing/saving state

--- html/index.php
+++ html/index.php
@@ -3,19 +3,19 @@
 <html>
 	<head>
 		<title>MusiClock</title>
-		<script type="text/javascript" src="js/playlist.js"></script>
+		<script type="text/javascript" src="js/musiclock.js"></script>
 		<link type="text/css" rel="stylesheet" href="css/main.css" />
-		<script>var list = new Playlist(<?=json_encode($pl->list)?>);window.onload=function(){list.init();list.setListControls(document.getElementById('list_controls'));};</script>
+		<script>var mc = new MusiClock(<?=json_encode($pl->list)?>);window.onload=function(){mc.init();mc.setListControls(document.getElementById('mc_controls'));};</script>
 	</head>
 	<body>
-		<section id="list_controls">
-			<span class="prev">&laquo; prev</span>
-			<span class="next">next &raquo;</span>
+		<section id="mc_controls">
 			<select id="moods">
 <?php foreach($pl->list as $mood => $set) : ?>
 				<option value="<?=$mood?>"><?=$mood?></option>
 <?php endforeach; ?>
 			</select>
+			<button class="prev">&laquo; prev</button>
+			<button class="next">next &raquo;</button>
 		</section>
 		<section id="list"></section>
 	</body>
--- html/js/musiclock.js
+++ html/js/musiclock.js
@@ -1,61 +1,97 @@
-var Playlist = function(list) {
+var MusiClock = function(list) {
 	this.list = list;
 };
-Playlist.prototype = {
+MusiClock.prototype = {
 	init: function() {
 		this.attachHandlers();
 		if (!this.restoreState()) {
-			this.checkTime();
+			var mood = this.getFirstMood();
+			this.update({
+				mood: mood,
+				playlist: this.getFirstPlaylist(mood),
+				track: 0,
+				time: 0,
+				volume: 1,
+				playing: false
+			});
+		}
+		this.checkTime();
+		this.startTrackingState();
+	},
+	saveState: function() {
+		if (window.localStorage && JSON) {
+			window.localStorage.state = JSON.stringify(this.state);
+		}
+		// FIXME: fall back to cookies
+	},
+	restoreState: function() {
+		var state;
+		if (window.localStorage && window.localStorage.state && JSON) {
+			state = JSON.parse(window.localStorage.state);
+		}
+		// FIXME: fall back to cookies
+
+		if (typeof state === "object") {
+			this.update(state);
+			return true;
+		}
+		return false;
+	},
+	startTrackingState: function() {
+		var $this = this;
+		this.stateInterval = setInterval(function() {
+			$this.saveState();
+		}, 1000);
+	},
+	stopTrackingState: function() {
+		clearInterval(this.stateInterval);
+	},
+	update: function(parameters) {
+		var diff, drawRequired, key, drawWorthy = ["mood","playlist"];
+		if (!this.state) {
+			this.state = parameters;
+			drawRequired = true;
+		}
+		for (key in parameters) {
+			if (this.state.hasOwnProperty(key)) {
+				this.state[key] = parameters[key];
+				if (this.state[key] !== parameters[key] && drawWorthy.indexOf(key) != -1) {
+					drawRequired = true;
+				}
+				switch(key) {
+					case "mood":
+						document.getElementById('moods').value = this.state.mood;
+						break;
+				}
+			}
+		}
+		if (!this.state.mood) this.state.mood = this.getFirstMood();
+		if (!this.state.playlist) this.state.playlist = this.getFirstPlaylist();
+		if (drawRequired) {
+			this.markupPlaylist();
+			this.gotoTrack(this.state.track);
 		}
 	},
 	attachHandlers: function() {
 		var $this = this;
 		document.getElementById('moods').onchange = function() {
-			$this.setMood(this.selectedOptions[0].value);
+			$this.update({mood: this.selectedOptions[0].value});
 		}
 		document.getElementsByTagName('body')[0].onkeyup = function(event) {
-			// console.log(event.keyCode);
+			console.log(event.keyCode);
 			switch(event.keyCode) {
+				case 38: $this.upVolume(); break;
+				case 40: $this.downVolume(); break;
 				case 32: $this.togglePause(); break;
 				case 75: $this.prevMood(); break;
 				case 74: $this.nextMood(); break;
 				case 72: $this.prevTrack(); break;
 				case 76: $this.nextTrack(); break;
+				default: return true;
 			}
+			event.preventDefault();
+			return false;
 		};
-	},
-	setMood: function(moodKey) {
-		if (!this.list.hasOwnProperty(moodKey))
-			return;
-		this.moodKey = moodKey;
-		document.getElementById('moods').value = moodKey;
-		this.setKey = null;
-		this.trackKey = -1;
-		this.checkTime();
-	},
-	setSet: function(setKey) {
-		if (!this.list[this.moodKey].hasOwnProperty(setKey))
-			return;
-		this.setKey = setKey;
-		this.markupSet();
-	},
-	saveState: function() {
-		if (window.localStorage) {
-			window.localStorage.moodKey = this.moodKey;
-			window.localStorage.setKey = this.setKey;
-			window.localStorage.trackKey = this.trackKey;
-		}
-		// FIXME: fall back to cookies
-	},
-	restoreState: function() {
-		if (window.localStorage) {
-			this.setMood(window.localStorage.moodKey);
-			// this.setKey = window.localStorage.setKey;
-			// this.trackKey = window.localStorage.trackKey;
-			return true;
-		}
-		// FIXME: fall back to cookies
-		return false;
 	},
 	/*
 	 * checks the time against the list keys, and automatically repeats on the
@@ -64,60 +100,42 @@ Playlist.prototype = {
 	 * expects keys like "1500" to mean 15:00 or 3:00pm
 	 */
 	checkTime: function() {
+		var $this = this, now = new Date(), hour, key, keys = [], nextHour, nextTime, wait;
+
 		clearTimeout(this.timeout);
-		var $this = this;
-		var now = new Date();
-		var hour = now.getHours().toString() + now.getMinutes().toString();
-		var keys = this.getNearestSetKeys(hour);
-		if (keys.length && keys[0] !== this.setKey) {
-			this.setSet(keys[0]);
+
+		hour = now.getHours().toString() + now.getMinutes().toString();
+		for (key in this.list[this.state.mood]) {
+			if (!this.list[this.state.mood][key].length) continue;
+			if (key <= hour) { keys[0] = key; }
+			else             { keys[1] = key; break; }
 		}
+
+		if (keys.length && keys[0] !== this.state.playlist) {
+			this.update({playlist: keys[0]});
+		}
+
 		if (keys.length > 1) {
-			var nextHour = keys[1].substr(0,2) + ':' + keys[1].substr(2,2) + ':00';
-			var nextTime = (new Date(now.toString().replace(/\d\d:\d\d:\d\d/, nextHour))).getTime();
-			var wait = nextTime - now.getTime();
+			nextHour = keys[1].substr(0,2) + ':' + keys[1].substr(2,2) + ':00';
+			nextTime = (new Date(now.toString().replace(/\d\d:\d\d:\d\d/, nextHour))).getTime();
+			wait = nextTime - now.getTime();
 			this.timeout = setTimeout(function() {
 				$this.checkTime();
 			}, wait);
 		}
 	},
-	/*
-	 * Returns an array of the last set key before or exactly on the given
-	 * time, and the set key which follows.
-	 *
-	 * (ignores empty sets)
-	 */
-	getNearestSetKeys: function(hour) {
-		var key, keys = [];
-		if (!this.moodKey) {
-			for (key in this.list) {
-				this.moodKey = key;
-				break;
-			}
-		}
-		for (key in this.list[this.moodKey]) {
-			if (!this.list[this.moodKey][key].length) continue;
-			if (key <= hour) {
-				keys[0] = key;
-			} else {
-				keys[1] = key;
-				break;
-			}
-		}
-		return keys;
-	},
-	markupSet: function() {
+	markupPlaylist: function() {
 		var $this = this,
 			markup = '',
-			set = this.list[this.moodKey][this.setKey];
+			playlist = this.list[this.state.mood][this.state.playlist];
 
-		markup += '<h2>' + this.moodKey + '</h2>';
-		for (var i = 0; i < set.length; i++) {
-			var file = set[i];
+		markup += '<h2>' + this.state.mood + '</h2>';
+		for (var i = 0; i < playlist.length; i++) {
+			var file = playlist[i];
 			var id = 'track_' + i;
 			markup += '<label for="' + id + '">' + file + '</label>'
 			+ '<audio id="' + id + '" controls>'
-			+ '	<source src="audio/' + this.moodKey + '/' + this.setKey + '/' + file + '" />'
+			+ '	<source src="audio/' + this.state.mood + '/' + this.state.playlist + '/' + file + '" />'
 			+ '</audio>';
 		}
 		var list = document.getElementById('list');
@@ -125,8 +143,15 @@ Playlist.prototype = {
 		var players = list.getElementsByTagName('audio');
 		for (var i = 0; i < players.length; i++) {
 			(function(i) {
+				players[i].addEventListener('canplay', function() {
+					if ($this.state.playing)
+						this.currentTime = $this.state.time;
+				});
 				players[i].addEventListener('play', function() {
-					$this.trackKey = i;
+					console.log('play ' + i);
+					$this.state.track = i;
+					$this.state.playing = true;
+					$this.state.insidePlay = true;
 					for (var j = 0; j < players.length; j++) {
 						if (i != j) {
 							var player = players[j];
@@ -136,55 +161,107 @@ Playlist.prototype = {
 							}
 						}
 					}
+					// FIXME: messy way to revert outside closure, after event
+					// chain finishes
+					setTimeout(function() { $this.state.insidePlay = false; }, 0);
+				});
+				players[i].addEventListener('pause', function() {
+					if (!$this.state.insidePlay) {
+						console.log('pause ' + i);
+						$this.state.playing = false;
+					}
+				});
+				players[i].addEventListener('timeupdate', function() {
+					$this.state.time = this.currentTime;
 				});
 				players[i].addEventListener('ended', function() {
+					console.log('end ' + i);
+					// FIXME: pause fires before ended; I'm assuming ended
+					// implies the audio WAS playing before paused, restoring
+					// state
+					$this.state.playing = true;
 					$this.nextTrack();
+				});
+				players[i].addEventListener('volumechange', function() {
+					$this.state.volume = this.state.volume;
 				});
 			})(i);
 		}
-		this.nextTrack();
 	},
-	gotoTrack: function(index) {
-		this.trackKey = index;
-		this.saveState();
-		document.getElementById('track_' + index).play();
+	getFirstMood: function() {
+		for (var mood in this.list) { return mood; }
+	},
+	getFirstPlaylist: function(mood) {
+		for (var playlist in this.list[mood || this.state.mood]) { return playlist; }
 	},
 	prevMood: function() {
-		var prevMoodKey;
-		for (var moodKey in this.list) {
-			if (prevMoodKey && moodKey == this.moodKey) break;
-			prevMoodKey = moodKey;
+		var prevMood;
+		for (var mood in this.list) {
+			if (prevMood && mood == this.state.mood) break;
+			prevMood = mood;
 		}
-		this.setMood(prevMoodKey);
+		this.update({mood: prevMood, playlist: null, track: 0, time: 0});
 	},
 	nextMood: function() {
-		var firstMoodKey, nextMoodKey;
-		for (var moodKey in this.list) {
-			if (!firstMoodKey)
-				firstMoodKey = moodKey;
-			if (nextMoodKey) {
-				nextMoodKey = moodKey;
+		var firstMood, nextMood;
+		for (var mood in this.list) {
+			if (!firstMood)
+				firstMood = mood;
+			if (nextMood) {
+				nextMood = mood;
 				break;
 			}
-			if (moodKey == this.moodKey)
-				nextMoodKey = moodKey;
+			if (mood == this.state.mood)
+				nextMood = mood;
 		}
-		if (moodKey == this.moodKey) nextMoodKey = firstMoodKey;
-		this.setMood(nextMoodKey);
+		if (mood == this.state.mood) nextMood = firstMood;
+		this.update({mood: nextMood, playlist: null, track: 0, time: 0});
+	},
+	gotoTrack: function(index) {
+		this.state.track = index;
+		tracks = document.getElementsByTagName('audio');
+		for (var i = 0; i < tracks.length; i++) {
+			var track = tracks[i];
+			// FIXME: add/remove class rather than replacing/wiping
+			if (i == index) {
+				track.volume = this.state.volume;
+				track.className = "active";
+				if (this.state.playing) {
+					track.play();
+				}
+			} else {
+				track.className = "";
+			}
+		}
 	},
 	prevTrack: function() {
-		var setLength = this.list[this.moodKey][this.setKey].length;
-		index = (isNaN(this.trackKey)) ? 0 : (this.trackKey - 1 + setLength) % setLength;
+		var setLength = this.list[this.state.mood][this.state.playlist].length;
+		index = (isNaN(this.state.track)) ? 0 : (this.state.track - 1 + setLength) % setLength;
 		this.gotoTrack(index);
 	},
 	nextTrack: function() {
-		var setLength = this.list[this.moodKey][this.setKey].length;
-		index = (isNaN(this.trackKey)) ? 0 : (this.trackKey + 1) % setLength;
+		var setLength = this.list[this.state.mood][this.state.playlist].length;
+		index = (isNaN(this.state.track)) ? 0 : (this.state.track + 1) % setLength;
 		this.gotoTrack(index);
 	},
 	togglePause: function() {
-		var audio = document.getElementById('track_' + this.trackKey);
-		audio.paused ? audio.play() : audio.pause();
+		var player = document.getElementById('track_' + this.state.track);
+		player.paused ? player.play() : player.pause();
+	},
+	setVolume: function(volume) {
+		if (typeof volume === "undefined" || isNaN(volume))
+			volume = this.state.volume;
+		else
+			this.state.volume = volume;
+
+		var player = document.getElementById('track_' + this.state.track);
+		if (player) player.volume = volume;
+	},
+	upVolume: function() {
+		this.setVolume(Math.min(1, this.state.volume + 0.1));
+	},
+	downVolume: function() {
+		this.setVolume(Math.max(0, this.state.volume - 0.1));
 	},
 	/*
 	 * add listeners on control element's children, by class
